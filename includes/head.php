f8e471dd2eec8ff613a24bb1e257b0eb5e4c935c
trollmarcus <trollmarcus@hotmail.com>
2019-05-15 19:37:48 +0100

    Update head.php

--- includes/head.php
+++ includes/head.php
@@ -1,4 +1,3 @@
 			<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
 			<link rel="stylesheet" type="text/css" href="./CSS/MainStyles.css">
 			<link rel="stylesheet" type="text/css" href="./CSS/bootstrap.css">
-			<link rel="stylesheet" type="text/css" href="./CSS/bootstrap.css">
